b7b7ad163344c2706b79110425ffcb045da74019
gpbaculio <gpbaculio@gmail.com>
2023-03-09 08:34:39 +0800

    integrate filter + sort on data

--- src/DessertsList.tsx
+++ src/DessertsList.tsx
@@ -3,13 +3,19 @@ import { DessertType } from "./App";
 
 type DessertsListProps = { data: DessertType[] };
 
+const filterDataByCalories = ({ calories }: DessertType) => calories < 500;
+const sortDataByCalories = (dessertA: DessertType, dessertB: DessertType) =>
+  dessertA.calories - dessertB.calories;
+
 function DessertsList({ data }: DessertsListProps) {
   // Implement the component here.
   const id = React.useId();
 
+  const result = data.filter(filterDataByCalories).sort(sortDataByCalories);
+
   return (
     <ul>
-      {data.map(({ name, calories }, index) => (
+      {result.map(({ name, calories }, index) => (
         <li key={`${index}-${id}`}>{`${name} - ${calories} cal`}</li>
       ))}
     </ul>
