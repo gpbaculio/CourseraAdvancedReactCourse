7d17acca30f00dd59ba4a5408b13071429cbb85c
gpbaculio <gpbaculio@gmail.com>
2023-03-09 23:46:27 +0800

    add landing ui

--- src/components/LandingSection.tsx
+++ src/components/LandingSection.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
+import useGetRequest from "../hooks/useGetRequest";
 
 const greeting = "Hello, I am Pete!";
 const bio1 = "A frontend developer";
@@ -8,15 +9,27 @@ const bio2 = "specialised in React";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
-const LandingSection = () => (
-  <FullScreenSection
-    justifyContent="center"
-    alignItems="center"
-    isDarkBackground
-    backgroundColor="#2A4365"
-  >
-
-  </FullScreenSection>
-);
+const LandingSection = () => {
+  return (
+    <FullScreenSection
+      justifyContent='center'
+      alignItems='center'
+      isDarkBackground
+      backgroundColor='#2A4365'>
+      <VStack>
+        <Avatar src='https://i.pravatar.cc/150?img=7' />
+        <Heading as='h1' size='md'>
+          {greeting}
+        </Heading>
+        <Heading as='h1' size='lg'>
+          {bio1}
+        </Heading>
+        <Heading as='h1' size='lg'>
+          {bio2}
+        </Heading>
+      </VStack>
+    </FullScreenSection>
+  );
+};
 
 export default LandingSection;
